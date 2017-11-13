8438c9a0e42bc66de1dc16ff1a78dabd012dfefc
qhx0807 <820240134@qq.com>
2017-11-13 17:33:13 +0800

    update :construction:

---
+++ src/components/page/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header" :style="{top: headerTop + 'px'}">
+    header menu
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'headermenu',
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState(['headerTop'])
+  },
+  created() {},
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+.header {
+  height: 100px;
+  width: 100%;
+  background-color: #fff;
+  transition: all 0.4s ease;
+  position: fixed;
+  top: 0;
+  z-index: 99;
+  //border-bottom: 1px solid #f5f5f5;
+  box-shadow: 0 1px 1px 0px #f8f8f8;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+const Header = resolve => require(['@/components/page/Header'], resolve)
+const Home = resolve => require(['@/components/page/Home'], resolve)
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/header',
+      name: 'Header',
+      component: Header
     }
   ]
 })
