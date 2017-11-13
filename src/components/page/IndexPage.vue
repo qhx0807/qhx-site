cb33ca4b4122a64d48fc073bc4aee0e10a1eb6f0
qhx0807 <820240134@qq.com>
2017-11-14 00:35:12 +0800

    add index page :sparkles:

---
+++ src/components/page/IndexPage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <Home></Home>
+    <div class="content">
+      <div class="notice">
+        <i class="iconfont icon-shuye">:</i>
+        <div class="notice-content">注意： 人生本来就没有相欠。别人对你付</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Home from './Home'
+export default {
+  name: 'index',
+  data() {
+    return {
+
+    }
+  },
+  components:{
+    Home
+  },
+  created () {
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.content{
+  max-width: 800px;
+  padding: 0 10px;
+  margin: 0 auto;
+  .notice{
+    margin-top: 30px;
+    padding: 20px;
+    border: 1px dashed #e6e6e6;
+    position: relative;
+    width: 100%;
+    margin-bottom: 20px;
+    background-color: #f8f8f8;
+    .icon-shuye{
+      color: #A0DAD0;
+      float: left;
+      margin-right: 10px;
+      vertical-align: bottom;
+      font-size: 20px;
+      padding-top: 2px;
+    }
+    .notice-content{
+      display: inline-block;
+      font-size: 14px;
+      color: #969696;
+      line-height: 24px;
+    }
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,16 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 const Header = resolve => require(['@/components/page/Header'], resolve)
-const Home = resolve => require(['@/components/page/Home'], resolve)
+const IndexPage = resolve => require(['@/components/page/IndexPage'], resolve)
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
+      name: 'IndexPage',
+      component: IndexPage
     },
     {
       path: '/header',
