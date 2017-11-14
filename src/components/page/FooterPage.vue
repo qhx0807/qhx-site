034694473fd4c2a42a2f11c9816b37213ac7f371
qhx0807 <820240134@qq.com>
2017-11-15 00:11:06 +0800

    :construction: add fouter

---
+++ src/components/page/FooterPage.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="footer">
+    <div class="foot-info">
+		  Copyright &copy; 2017	.All rights reserved.	
+      <span> | </span>
+      <a href="www.qhxing.site">qhx's Zone</a>
+      <span> | </span>
+      Theme frome <a href="">fuzzz.</a>
+      <div><a href="">网站统计</a></div>
+      <div>渝ICP备xxxx</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'FooterPage',
+  data () {
+    return {
+      
+    }
+  },
+  created () {
+    
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.footer{
+  clear: both;
+  padding: 2%;
+  margin-top: 12px;
+  background-color: #fff;
+  border-top: 1px solid #f7f7f7;
+  .foot-info{
+    text-align: center;
+    font-size: 13px;
+    color: #B9B9B9;
+    a{
+      color: #848484;
+      &:hover{
+        color: #A0DAD0;
+      }
+    }
+  }
+}
+</style>
+
+
