a29b9ac8d2bc30381aa38f5ed6dc164c0646c0e3
qhx0807 <820240134@qq.com>
2017-11-16 22:38:42 +0800

    update admin home page

--- src/components/admin/Admin.vue
+++ src/components/admin/Admin.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="admin">
-    <Button type="primary">Primary</Button>
+    <div class="sidebar-menu-con" :style="{width: hideMenuText?'60px':'200px', overflow: hideMenuText ? 'visible' : 'auto',}">
+      <SidebarMenu></SidebarMenu>
+    </div>
+    <div class="admin-content">
+      <div class="con-header"></div> 
+    </div>
   </div>
 </template>
 
 <script>
+import SidebarMenu from './SidebarMenu.vue'
+import SidebarMenuShrink from './SidebarMenuShrink.vue'
 export default {
   name: 'admin',
   data() {
     return {
-      name: '',
-      psd: '',
+      hideMenuText: false,
+      hideMenuText: false,
     }
+  },
+  components: {
+    SidebarMenu
   },
   created() {
 
@@ -23,7 +33,28 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.admin{
-  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+.admin {
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: row;
+  background: url('../../assets/bg.png') repeat;
+  .sidebar-menu-con {
+    height: 100%;
+    background: rgb(73, 80, 96);
+  }
+  .admin-content{
+    flex: 1;
+    .con-header{
+      height: 60px;
+      width: 100%;
+      position: fixed;
+      background-color: #fff;
+      z-index: 99;
+      box-shadow: 0 2px 1px 1px rgba(99, 99, 99, 0.1);
+    }
+  }
 }
 </style>
