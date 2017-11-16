635384af3cb798615213d72afe2dd50face7f4ce
qhx0807 <820240134@qq.com>
2017-11-16 21:47:27 +0800

    add admin home page

---
+++ src/components/admin/Admin.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="admin">
+    <Button type="primary">Primary</Button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'admin',
+  data() {
+    return {
+      name: '',
+      psd: '',
+    }
+  },
+  created() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.admin{
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+}
+</style>
