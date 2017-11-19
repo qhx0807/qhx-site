55d243a0543f06b151aaae56ab2f4f63d0d06fea
qhx0807 <820240134@qq.com>
2017-11-20 00:14:13 +0800

    :sparkles: add md editor

--- src/components/admin/Publish.vue
+++ src/components/admin/Publish.vue
@@ -1,22 +1,53 @@
 <template>
   <div class="publish">
-    publish
+    <Row>
+      <Col span="19">
+        <Form ref="formInline" :model="formInline" :rules="ruleInline">
+          <FormItem prop="" label="文章标题" :label-width="80">
+            <Input type="text" v-model="formInline.user">
+            </Input>
+          </FormItem>
+        </Form>
+        <mavon-editor :ishljs="true" @change="onInputMd" :value="value" code-style="code-hybrid" style="height: 600px"></mavon-editor>
+      </Col>
+      <Col span="5" style="padding-left:10px;">
+      <Card :bordered="false">
+        <p slot="title">
+          <Icon type="ios-pricetags-outline"></Icon>
+          标签
+        </p>
+        <div>
+          html
+        </div>
+      </Card>
+      </Col>
+    </Row>
   </div>
 </template>
 
 <script>
+import { mavonEditor } from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
 export default {
   name: 'publish',
   data() {
     return {
-
+      value: '',
+      htmlCode: '',
+      formInline:{},
+      ruleInline:[],
     }
+  },
+  components: {
+    mavonEditor
   },
   created() {
 
   },
   methods: {
-
+    onInputMd(val, render) {
+      this.htmlCode = render
+    },
   }
 }
 </script>
@@ -24,5 +55,9 @@ export default {
 <style lang="less" scoped>
 .publish {
   padding: 6px;
+  height: 580px;
+  .v-note-wrapper {
+    z-index: 99;
+  }
 }
 </style>
