2093d8e878accab09cf7aeb824b63a453fe497dd
qhx0807 <820240134@qq.com>
2017-11-19 21:38:06 +0800

    del hello

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 const Header = resolve => require(['@/components/page/Header'], resolve)
 const IndexPage = resolve => require(['@/components/page/IndexPage'], resolve)
 const Tags = resolve => require(['@/components/page/Tags'], resolve)
