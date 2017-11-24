5d1918d3252fa614d079fd54501f241c7a0afcc9
qhx0807 <820240134@qq.com>
2017-11-24 15:22:38 +0800

    update

--- src/components/admin/Manager.vue
+++ src/components/admin/Manager.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="manager">
-    manager
+    <Card :bordered="false">
+      <Table :columns="tableColumns" :data="tableData"></Table>
+    </Card>
   </div>
 </template>
 
@@ -9,7 +11,41 @@ export default {
   name: 'manager',
   data() {
     return {
-
+      tableData:[],
+      tableColumns:[
+        {
+          title:'登录名',
+          key:'name',
+        },
+        {
+          title:'密码',
+          key:'pwd',
+        },
+        {
+          title:'上次登录时间',
+          key:'lastLoginTime',
+        },
+        {
+          title:'上次登录IP',
+          key:'ip',
+        },
+        {
+          title:'上次登录地点',
+          key:'location',
+        },
+        {
+          title:'角色',
+          key:'role',
+        },
+        {
+          title:'头像',
+          key:'headImg',
+        },
+         {
+          title:'操作',
+          key:'_id',
+        },
+      ],
     }
   },
   created() {
@@ -24,5 +60,15 @@ export default {
 <style lang="less" scoped>
 .manager {
   padding: 6px;
+  position: relative;
+  .ivu-table-wrapper {
+    border: none!important;
+  }
+  .ivu-table:before {
+    height: 0;
+  }
+  .ivu-table:after {
+    width: 0;
+  }
 }
 </style>
