7314e4c1a5d29affff9e4153a48230c0b6b2bff5
qhx0807 <820240134@qq.com>
2017-11-26 23:32:14 +0800

    update publish

--- src/components/admin/Publish.vue
+++ src/components/admin/Publish.vue
@@ -2,13 +2,13 @@
   <div class="publish">
     <Row>
       <Col span="19">
-        <Form ref="formInline" :model="formInline" :rules="ruleInline">
+        <Form ref="formInline" :model="publishObj" :rules="ruleInline">
           <FormItem prop="" label="文章标题" :label-width="80">
-            <Input type="text" v-model="formInline.user">
+            <Input type="text" v-model="publishObj.title">
             </Input>
           </FormItem>
         </Form>
-        <mavon-editor :ishljs="true" @change="onInputMd" :value="value" code-style="code-hybrid" style="height: 600px"></mavon-editor>
+        <mavon-editor :ishljs="true" @change="onInputMd" :value="publishObj.mdValue" code-style="code-hybrid" style="height: 700px"></mavon-editor>
       </Col>
       <Col span="5" style="padding-left:10px;">
       <Card :bordered="false">
@@ -26,7 +26,7 @@
           标签
         </p>
         <div>
-          html
+          <span class="tag-span" @click="onSlectTags(item.name, index)" :class="{aactive:isSelect[index]}" v-for="(item, index) in tagsData" :key="item._id">{{item.name}}</span>
         </div>
       </Card>
       <Card :bordered="false" style="margin-top:12px;">
@@ -35,9 +35,18 @@
           归档
         </p>
         <div>
-          <Select v-model="archive">
-              <Option v-for="item in archData" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          <Select v-model="publishObj.archive">
+              <Option v-for="item in archData" :value="item.name" :key="item._id">{{ item.name }}</Option>
           </Select>
+        </div>
+      </Card>
+      <Card :bordered="false" style="margin-top:12px;">
+        <p slot="title">
+          <Icon type="ios-gear-outline"></Icon>
+          动作
+        </p>
+        <div>
+          <Button type="primary" @click="publishAction">发布文章</Button>
         </div>
       </Card>
       </Col>
@@ -46,6 +55,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+import moment from 'moment'
 import { mavonEditor } from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
 export default {
@@ -56,19 +67,59 @@ export default {
       htmlCode: '',
       formInline:{},
       ruleInline:{},
-      archive:'',
       archData:[],
+      tagsData:[],
+      publishObj:{
+        archive:'',
+        title:'',
+        htmlvalue:'',
+        mdValue:'',
+        date:'',
+        titleImg:'',
+        auth: sessionStorage.name,
+        like: 0,
+        watch: 0,
+        commits:[],
+      },
+      isSelect:[],
     }
   },
   components: {
     mavonEditor
   },
   created() {
-
+    this.getTagsList()
+    this.getArchList()
   },
   methods: {
     onInputMd(val, render) {
-      this.htmlCode = render
+      this.publishObj.htmlvalue = render
+      this.publishObj.mdValue = val
+    },
+    getTagsList() {
+      axios.get(API_URL + '/tags').then(function(response) {
+        this.tagsData = response.data.Data
+        this.spinShow = false
+      }.bind(this)).catch(function(error) {
+        this.spinShow = false
+      }.bind(this))
+    },
+    getArchList() {
+      axios.get(API_URL + '/archive').then(function(response) {
+        this.archData = response.data.Data
+        this.spinShow = false
+      }.bind(this)).catch(function(error) {
+        this.spinShow = false
+      }.bind(this))
+    },
+    publishAction(){
+      this.publishObj.date = moment().format().replace('T', ' ').substring(0,19)
+      console.log(this.publishObj)
+    },
+    onSlectTags(name,index){
+      alert(index)
+      this.isSelect[index] = true
+      console.log(this.isSelect)
     },
   }
 }
@@ -83,6 +134,15 @@ export default {
   }
   .head-img{
     height: 100px;
+  }
+  .tag-span{
+    padding: 4px 8px;
+    border: 1px solid #eee;
+    margin-right: 8px;
+    cursor: pointer;
+    &.active{
+      border: px solid #444;
+    }
   }
 }
 </style>
