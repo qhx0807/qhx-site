b7d4f8bfe634f990071fb9f6d9b1a86d7499a2a9
qhx0807 <820240134@qq.com>
2017-12-07 22:37:30 +0800

    :sparkles: add photo

---
+++ src/components/page/Photo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Home></Home>
+    <div>
+      photo
+    </div>
+    <FooterPage></FooterPage>
+  </div>
+  
+</template>
+
+<script>
+import Home from './Home'
+import FooterPage from './FooterPage'
+import axios from 'axios'
+export default {
+  name: 'photo',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    Home,
+    FooterPage,
+  },
+  created() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
