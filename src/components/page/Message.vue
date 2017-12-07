4596f078101ffec36bfd24c1a8479cc3d4e7d3a2
qhx0807 <820240134@qq.com>
2017-12-07 22:37:10 +0800

    :sparkles: add msg

---
+++ src/components/page/Message.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Home></Home>
+    <div>
+      msg
+    </div>
+    <FooterPage></FooterPage>
+  </div>
+  
+</template>
+
+<script>
+import Home from './Home'
+import FooterPage from './FooterPage'
+import axios from 'axios'
+export default {
+  name: 'msg',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    Home,
+    FooterPage,
+  },
+  created() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
